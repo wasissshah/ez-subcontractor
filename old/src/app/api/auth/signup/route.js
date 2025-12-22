9f30f4cbc0d6ccb8d8605995336208ee394caa7c
wasissshah <wasissshah@gmail.com>
2025-12-23 04:26:45 +0500

    changes

--- old/src/app/api/auth/signup/route.js
+++
@@ -1,7 +0,0 @@
-export async function POST(req) {
-  const body = await req.json();
-  return new Response(JSON.stringify({ message: "Signup successful", user: body }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
